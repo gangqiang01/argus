ec28e4b16e690efe29c935054e6e39b06d1fc219
gangqiang.sun <gangqiang.sun@advantech.com.cn>
2018-09-20 10:40:04 +0800

    rm *.html

--- assets/js/changeurl.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-sed  -i  's/\/golang/http:\/\/172.21.73.144:9090/g'  `grep "\/golang" -rl --exclude={recoveryurl.sh,changeurl.sh}`
--- assets/js/recoveryurl.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-sed  -i  's/http:\/\/172.21.73.144:9090/\/golang/g'  `grep "http:\/\/172.21.73.144:9090"  -rl --exclude={recoveryurl.sh,changeurl.sh}`
